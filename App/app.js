5efdafdaab2be856e23fea932c217ed79a383e4c
CityRoys <CityRoys.ua@gmail.com>
2021-11-30 17:49:13 +0200

    faq and contacts

--- App/app.js
+++ App/app.js
@@ -1,23 +1,47 @@
 const animItems = document.querySelectorAll("._animation");
 
 if (animItems.length > 0) {
+  window.addEventListener("scroll", animScroll);
   function animScroll() {
     for (let index = 0; index < animItems.length; index++) {
       const animItem = animItems[index];
       const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("active");
+      } else {
+        if (!animItem.classList.contains("hide_anim")) {
+          animItem.classList.remove("active");
+        }
+      }
     }
   }
 
-  function offser(el) {
+  function offset(el) {
     const rect = el.getBoundingClientRect(),
       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     return {
-      top: ReadableStreamDefaultController.top + scrollTop,
+      top: rect.top + scrollTop,
       left: rect.left + scrollLeft,
     };
   }
+
+  setTimeout(() => {
+    animScroll();
+  }, 500);
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
